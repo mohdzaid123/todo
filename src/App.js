d75e45493f3da44474c13e674e325d1190a0fd1a
mohdzaid123 <www.mohdzaid2901@gmail.com>
2022-06-28 15:42:11 +0530

    Todo

--- src/App.js
+++ src/App.js
@@ -1,121 +1,38 @@
-import React, { useState } from "react";
-
-import Icon from "./components/Icon";
-
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
-import "bootstrap/dist/css/bootstrap.css";
+import React, { useState, useEffect } from "react";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-const itemArray = new Array(9).fill("empty");
+import Todos from "./components/Todos";
+import TodoForm from "./components/Todoform";
 
 const App = () => {
-  const [isCross, setIsCross] = useState(false);
-  const [winMessage, setWinMessage] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const reloadGame = () => {
-    setIsCross(false);
-    setWinMessage("");
-    itemArray.fill("empty", 0, 9);
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+    if (localTodos) {
+      setTodos(JSON.parse(localTodos));
+    }
+  }, []);
+
+  const addTodos = async (todo) => {
+    setTodos([...todos, todo]);
   };
 
-  const checkIsWinner = () => {
-    if (
-      itemArray[0] === itemArray[1] &&
-      itemArray[0] === itemArray[2] &&
-      itemArray[0] !== "empty"
-    ) {
-      setWinMessage(`${itemArray[0]} wins`);
-    } else if (
-      itemArray[3] !== "empty" &&
-      itemArray[3] === itemArray[4] &&
-      itemArray[4] === itemArray[5]
-    ) {
-      setWinMessage(`${itemArray[3]} won`);
-    } else if (
-      itemArray[6] !== "empty" &&
-      itemArray[6] === itemArray[7] &&
-      itemArray[7] === itemArray[8]
-    ) {
-      setWinMessage(`${itemArray[6]} won`);
-    } else if (
-      itemArray[0] !== "empty" &&
-      itemArray[0] === itemArray[3] &&
-      itemArray[3] === itemArray[6]
-    ) {
-      setWinMessage(`${itemArray[0]} won`);
-    } else if (
-      itemArray[1] !== "empty" &&
-      itemArray[1] === itemArray[4] &&
-      itemArray[4] === itemArray[7]
-    ) {
-      setWinMessage(`${itemArray[1]} won`);
-    } else if (
-      itemArray[2] !== "empty" &&
-      itemArray[2] === itemArray[5] &&
-      itemArray[5] === itemArray[8]
-    ) {
-      setWinMessage(`${itemArray[2]} won`);
-    } else if (
-      itemArray[0] !== "empty" &&
-      itemArray[0] === itemArray[4] &&
-      itemArray[4] === itemArray[8]
-    ) {
-      setWinMessage(`${itemArray[0]} won`);
-    } else if (
-      itemArray[2] !== "empty" &&
-      itemArray[2] === itemArray[4] &&
-      itemArray[4] === itemArray[6]
-    ) {
-      setWinMessage(`${itemArray[2]} won`);
-    }
-  };
-  const changeItem = (itemNumber) => {
-    if (winMessage) {
-      return toast(winMessage, { type: "success" });
-    }
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    if (itemArray[itemNumber] === "empty") {
-      itemArray[itemNumber] = isCross ? "cross" : "circle";
-      setIsCross(!isCross);
-    } else {
-      return toast("Already filled", { type: "error" });
-    }
-    checkIsWinner();
+  const markComplete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
-    <Container className="p-5">
-      <ToastContainer position="bottom-center" />
-      <Row>
-        <Col md={6} className="offset-md-3">
-          {winMessage ? (
-            <div className="mb-2 mt-2">
-              <h1 className="text-success text-uppercase text-center">
-                {winMessage}
-              </h1>
-              <Button color="success" block onClick={reloadGame}>
-                Relaod the game
-              </Button>
-            </div>
-          ) : (
-            <h1 className="text-center text-warning">
-              {isCross ? "Cross" : "Circle"} turns
-            </h1>
-          )}
-          <div className="grid">
-            {itemArray.map((item, index) => (
-              <Card color="warning" onClick={() => changeItem(index)}>
-                <CardBody className="box">
-                  <Icon name={item} />
-                </CardBody>
-              </Card>
-            ))}
-          </div>
-        </Col>
-      </Row>
+    <Container fluid>
+      <h1>Todo with local storage</h1>
+      <Todos todos={todos} markComplete={markComplete} />
+      <TodoForm addTodos={addTodos} />
     </Container>
   );
 };
